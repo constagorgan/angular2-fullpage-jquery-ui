0af4fa1830c51c37c3ffb6b4a51df4248ba603e7
ConstaGorgan <consta.gorgan@gmail.com>
2016-12-14 22:34:41 +0200

    Added jquery & jquery-ui + touch-punch

--- src/app/float-img/float-img.component.ts
+++ src/app/float-img/float-img.component.ts
@@ -1,15 +1,20 @@
-import { Component, OnInit } from '@angular/core';
+import { ViewChild, Component, OnInit, AfterViewInit } from '@angular/core';
+declare var jQuery:any;
 
 @Component({
   selector: 'float-img',
   templateUrl: './float-img.component.html',
   styleUrls: ['./float-img.component.css']
 })
-export class FloatImgComponent implements OnInit {
-
-  constructor() { }
+export class FloatImgComponent implements OnInit, AfterViewInit {
+  @ViewChild('resizableDiv') resizableDiv;
 
   ngOnInit() {
+
+  }
+
+  ngAfterViewInit() {
+    jQuery(this.resizableDiv.nativeElement).resizable();
   }
 
 }
